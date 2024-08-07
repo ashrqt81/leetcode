fecb7e7c11d48528dd74905e5a9cce44c81a09b5
Ashrqt abdelhamed <85971759+ashrqt81@users.noreply.github.com>
2024-08-07 02:45:10 -0500

    Time: 369 ms (96.52%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,13 +1,9 @@
-SELECT id
-FROM (
-    SELECT 
-        id, 
-        temperature, 
-        recordDate,
-        LAG(temperature) OVER (ORDER BY recordDate) AS previous_temp,
-        LAG(recordDate) OVER (ORDER BY recordDate) AS previous_date
-    FROM weather
-) AS TempWithLag
-WHERE 
-    temperature > previous_temp
-    AND DATEDIFF(day, previous_date, recordDate) = 1;
+select id
+from (
+    select id,temperature,recordDate,
+    lag(temperature) over(order by recordDate ) as lapTemp,
+    lag(recordDate) over (order by recordDate) as prevDate
+    from weather
+) as ggg
+where lapTemp<temperature
+and datediff(day,prevDate,recordDate)=1
